0decce3016e68bc93f47b4f23f7c20c49bcf8bf6
speaker73 <speaker73@ukr.net>
2016-09-28 19:33:17 +0300

    add some js

--- js/navscroll.js
+++ js/navscroll.js
@@ -7,3 +7,10 @@ $(window).scroll(function() {
 });
  $('nav').toggleClass("when_scroll", scroll_nav());
  $('whitediv').toggleClass("white_block", scroll_nav());
+
+ $('.pq-list>li').click(function () {
+     $(this).toggleClass("active");
+  });
+
+
+ 
