08dbbfa469bfde298cc3341128894b9791135f9b
Vlad <speaker73.mieln@gmail.com>
2017-06-03 23:02:31 +0300

    fix list show&hide modul

--- js/navscroll.js
+++ js/navscroll.js
@@ -9,31 +9,23 @@ $(window).scroll(function() {
  $('whitediv').toggleClass("white_block", scroll_nav());
 
  $('.pq-list>li').click(function () {
+ 	$('.pq-list>li').each(function(id, el){
+ 		if($(el).hasClass('active')){
+ 			$(el).removeClass('active');
+ 			$(el).find('div').hide("fast");
+ 		}
+ 	});
  	$(this).toggleClass("active");
  	if ($(".pq-list>li.active>div").is(":hidden")) {
         $(".pq-list>li.active>div").slideDown("slow");
       }
       else{
+      	$(this).removeClass('active');
       	$('div', this).hide("fast");
       }
    });
-var actionURL = "https://docs.google.com/forms/d/e/1FAIpQLSdAMior_2KBXF_k8n8uPBmyf8YiVRjll6PZHXD7X91XUaNW6Q/formResponse?";
-var data = {
-	"entry.1210792036":'Vlad'
-};
-/*$('.enjoy_btn').on('click', function(){
-		$('#google_form').find('.form-control').each(function(id, obj){
-			data[obj.name] = $(obj).val()
-		});
-		data['submit'] = 'Submit';
-		console.log('data', data);
-	$.post(actionURL,data,function(){
-		console.log('message compleate')
-	});
 
 
-});
-*/
 $('.enjoy_btn').on('click', function(){
 		$('.message_submit').text('Message is submit!')
 })
